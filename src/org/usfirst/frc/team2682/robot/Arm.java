82b769ec147c4566b008a55600efd8e4f68b94a3
Christopher Lindbeck <clindbeck@gmail.com>
2015-01-10 10:55:23 -0500

    comments

--- src/org/usfirst/frc/team2682/robot/Arm.java
+++ src/org/usfirst/frc/team2682/robot/Arm.java
@@ -1,5 +1,51 @@
 package org.usfirst.frc.team2682.robot;
 
-public class Arm {
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 
+
+public class Arm extends Object{
+//Represents a limited rotation arm run with a relay and two limit switches.
+	DigitalInput fLimit;
+	DigitalInput rLimit;
+	Relay op;
+	
+	public Arm(DigitalInput forward, DigitalInput backward, Relay operator) {
+		fLimit = forward;
+		rLimit = backward;
+		op = operator;
+		
+	}
+	
+	public void update(boolean forward, boolean back){
+		
+		
+		if(forward && !fLimit.get()){
+			
+			op.set(Relay.Value.kForward);
+			
+		} else if (back && !rLimit.get()){
+			
+			
+			
+			
+		} else {
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/org/usfirst/frc/team2682/robot/Robot.java
+++ src/org/usfirst/frc/team2682/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 
 /**
@@ -20,7 +22,13 @@ public class Robot extends IterativeRobot {
 	RobotDrive chassis1;
 	Timer time;
 	Encoder nCode;
+	Relay armMotor;
+	DigitalInput fStop;
+	DigitalInput bStop;
+	Timer revTimer;
 	
+	boolean sweepForward;
+	boolean sweepBackward;
     
     public void robotInit() {
     	
@@ -29,6 +37,10 @@ public class Robot extends IterativeRobot {
     	time = new Timer();
     	nCode = new Encoder(1,2);
     	
+    	armMotor = new Relay(1);
+    	fStop = new DigitalInput(1);
+    	bStop = new DigitalInput(2);
+    	
     	
     }
     
@@ -36,23 +48,29 @@ public class Robot extends IterativeRobot {
      * This function is run once each time the robot enters autonomous mode
      */
     public void autonomousInit() {
+    	//start the robot at half speed
     	chassis1.arcadeDrive(0.5,0);
     	
-    	while(nCode.get() < 700){
+    	
+    	/*
+    	//delay until encoder is over a value
+    	while(nCode.get() < 3000){
     		time.delay(0.01);
     	}
     	
+    	//Stop the robot
     	chassis1.arcadeDrive(0,0);
-    	
+    	*/
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	
-    	//if(nCode.get() > 3000)
-    	//chassis1.arcadeDrive(0.5,0);
+    	//This version of the encoder code is more clear.
+    	if(nCode.get() > 3000)
+    		chassis1.arcadeDrive(0,0);
+    		
     }
     
     /**
@@ -68,7 +86,46 @@ public class Robot extends IterativeRobot {
     
     chassis1.arcadeDrive(joy1.getRawAxis(1),joy1.getRawAxis(2));	
     	
+    //If button 6 is pressed and we are not already moving forward then move forward. Also wipe the timer and set backwards value to false to avoid conflictions.
+    if(joy1.getRawButton(6) && !sweepForward){
+    	sweepForward = true;
+    	sweepBackward = false;
+    	revTimer.stop();
+    	revTimer.reset();
     	
+    }
+    
+    
+    //check the boolean values and set the relay accordingly
+    if(sweepForward == true){
+    	
+    	armMotor.set(Relay.Value.kForward);
+    	
+    } else if (sweepBackward) {
+    	
+    	armMotor.set(Relay.Value.kForward);
+    }
+    
+    //If the arm is moving forward and the fStop limit is depressed then stop the arm and start the reversal delay timer
+    if(sweepForward && fStop.get()){
+    	armMotor.set(Relay.Value.kOff);
+    	revTimer.start();
+    	sweepForward = false;
+    }
+    //If the reversal delay timer has been running longer then the delay then start the arm in the other direction and wipe the timer
+    if (revTimer.get() > 0.25){
+    	sweepBackward = true;
+    	revTimer.stop();
+    	revTimer.reset();
+    	
+    }
+    
+    //if the arm is moving backwards and the rStop limit is depressed then stop the motor and set the value to false
+    if(sweepBackward && bStop.get()){
+    	armMotor.set(Relay.Value.kOff);
+    	sweepBackward = false;
+    }
+    
     }
     
     /**
